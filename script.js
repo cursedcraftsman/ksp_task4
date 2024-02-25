edd219471fc363fcef641b50b3081778c523f745
Vyshnavi A <156635517+cursedcraftsman@users.noreply.github.com>
2024-02-25 10:44:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("hello all");
+alert("registrations are closing soon!!");
+//variables 
+let name="vyshnavi";
+let age=20;
+let Adult=true;
+let height=5.6;
+console.log(age);
+console.log(Adult);
+console.log(height);
+
+//data types
+console.log(typeof(age));
+console.log(typeof(Adult));
+console.log(typeof(height));
